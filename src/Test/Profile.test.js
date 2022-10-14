4920b32626172c25110dee0d594b46666937b0fd
Alaashaheen2 <alaa.m.shaheen22@gmail.com>
2022-10-15 03:09:36 +0430

    refactore tests

--- src/Test/Profile.test.js
+++ src/Test/Profile.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import Profile from '../components/Profile/Profile';
 import store from '../redux/configureStore';
-import Rockets from '../components/Rockets/Rockets';
 
-describe('Test if: ', () => {
-  it('Rockets component renders', () => {
+describe('Testing if: ', () => {
+  it('Profile view renders reserved rockets', () => {
     const box = render(
       <React.StrictMode>
         <Provider store={store}>
-          <Rockets />
+          <Profile />
         </Provider>
       </React.StrictMode>,
     );
