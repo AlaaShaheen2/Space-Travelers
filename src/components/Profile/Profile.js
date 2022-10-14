2951031fa8e808c7c42dfef579964dbad984156d
Alaashaheen2 <alaa.m.shaheen22@gmail.com>
2022-10-15 02:43:41 +0430

    Display reserved rocket into profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,32 @@
-const Profile = () => (
-  <h3>Profile</h3>
-);
+import './Profile.css';
+import { useSelector } from 'react-redux';
+import Table from 'react-bootstrap/Table';
+import { Col, Row } from 'react-bootstrap';
+
+const Profile = () => {
+  const rocketsData = useSelector((state) => state.rockets);
+  const reservedRockets = rocketsData.filter((rocket) => rocket.reserved);
+
+  return (
+    <Row className="row">
+      <Col>
+        <h4 className="title">Missions</h4>
+      </Col>
+      <Col>
+        <h4 className="title">Rockets</h4>
+        <Table striped bordered variant="danger">
+          <tbody>
+            {reservedRockets.map((rocket) => (
+              <tr key={rocket.rocket_id}>
+                <td className="reserved">
+                  {rocket.rocket_name}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
+};
 export default Profile;
